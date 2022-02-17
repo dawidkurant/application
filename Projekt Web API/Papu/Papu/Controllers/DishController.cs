963240576e08c0e328f02a5c6df8a237535414b1
dawidkurant <dawidkurant15@gmail.com>
2022-02-17 17:20:40 +0100

    Adding the DishController

---
+++ Projekt Web API/Papu/Papu/Controllers/DishController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Papu.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Papu.Controllers
+{
+    [Route("api/dish")]
+    public class DishController : ControllerBase
+    {
+        private readonly PapuDbContext _dbContext;
+
+        public DishController(PapuDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //pobranie wszystkich potraw z bazy i zwrócenie ich do klienta z kodem 200 czyli OK
+        public ActionResult<IEnumerable<Dish>> GetAll()
+        {
+            var dishes = _dbContext
+                .Dishes
+                .ToList();
+
+            return Ok(dishes);
+        }
+    }
+}
